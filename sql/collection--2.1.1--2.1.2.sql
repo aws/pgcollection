9aeb106b82855f22bba208ab1b43eecd58ebfd8e
User <user@example.com>
2026-05-12 12:48:49 -0400

    Bump version to 2.1.2

---
+++ sql/collection--2.1.1--2.1.2.sql
@@ -0,0 +1,4 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION collection UPDATE TO '2.1.2'" to load this file. \quit
+
+-- No schema changes in this version bump
